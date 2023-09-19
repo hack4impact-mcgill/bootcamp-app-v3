ea2c641460f213fac44ae449de1128810260e095
Sophearah Suy-Puth <sophearahspsp@gmail.com>
2023-09-19 13:12:42 -0400

    delete button for frontend demo

--- frontend/src/components/TodoListItem.tsx
+++ frontend/src/components/TodoListItem.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { Checkbox, FormControlLabel, Box, IconButton } from '@mui/material';
-import ClearIcon from '@mui/icons-material/Clear';
-import { deleteTodo, updateTodo } from '../services';
+import { Checkbox, FormControlLabel, Box } from '@mui/material';
+import { updateTodo } from '../services';
 import {useTodoStore} from './TodoApp';
 import {TodoI, TodoStore} from '../types/Todo';
+import DeleteButton from './DeleteButton';
 
 // Component takes in a prop called 'todo' that is of type 'TodoI'
 const TodoItem = (props: { todo: TodoI }) => {
     const todos = useTodoStore((state: TodoStore) => state.todos)
     const setTodos = useTodoStore((state: TodoStore) => state.setTodos)
-
-    // Function to handle clicking on the delete button
-	const onClickDelete = async () => {
-        // If this TodoItem doesn't have an id, throw an error
-        if (!props.todo.id) throw "Nothing to delete";
-
-        // Call deleteTodo() and wait until we recieve response
-        const response = await deleteTodo(props.todo.id);
-        
-        // If response is OK (status == 200), use setTodos to update the state of the todos
-        if (response.status == 200) {
-            // Use filter to create a new array that contains a subset of the array that doesn't have this todoItem's id
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-            // (all javascript is valid typescript)
-            setTodos(todos.filter((todoItem) => todoItem.id !== props.todo.id));
-        }
-	};
 
     // Function to handle clicking on the checkbox
     const onClickToggle = async () => {
@@ -70,11 +53,7 @@ const TodoItem = (props: { todo: TodoI }) => {
                 }}
             />
             {/* Delete button */}
-            <IconButton
-                // onClick (event triggers when button is clicked) is set to the onClickDelete() function
-                onClick={onClickDelete} color="error" aria-label="delete">
-                <ClearIcon />
-            </IconButton>
+            <DeleteButton todo={props.todo}/>
         </Box>
     )
 }
